abd68a547ad659d1788cdb7a1c3e64652115cd40
Rapunzel <osina_kv@mail.ru>
2025-07-09 22:26:53 +0300

    add clients views to view details, list, create, update and delete of clients and urls

--- newsletter/urls.py
+++ newsletter/urls.py
@@ -2,9 +2,15 @@ from django.urls import path
 
 import newsletter
 from newsletter.apps import NewsletterConfig
+from newsletter.views import ClientListView, ClientCreateView, ClientDetailView, ClientUpdateView, ClientDeleteView
 
 app_name = NewsletterConfig.name
 
 urlpatterns = [
     path("", newsletter, name="newsletter"),
+    path('clients/', ClientListView.as_view(), name='client_list'),
+    path('clients/create/', ClientCreateView.as_view(), name='client_create'),
+    path('clients/<int:pk>/', ClientDetailView.as_view(), name='client_detail'),
+    path('clients/<int:pk>/update/', ClientUpdateView.as_view(), name='client_update'),
+    path('clients/<int:pk>/delete/', ClientDeleteView.as_view(), name='client_delete'),
 ]
